b9d617815669c1efd7c4f20e307224315655b331
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2023-04-20 22:06:48 +0300

    baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: "https://web3mining.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/web3mining.ton/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
