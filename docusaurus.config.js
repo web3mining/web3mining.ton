06d2d36e1c76846eee79eca9d2de388902235d1c
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2023-05-06 13:17:47 +0300

    Label

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,11 +74,11 @@ const config = {
             type: "docSidebar",
             sidebarId: "tutorialSidebar",
             position: "left",
-            label: "Tutorial",
+            label: "Docs",
           },
           { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/web3mining/web3mining.ton",
             label: "GitHub",
             position: "right",
           },
@@ -91,7 +91,7 @@ const config = {
             title: "Docs",
             items: [
               {
-                label: "Tutorial",
+                label: "Docs",
                 to: "/docs/intro",
               },
             ],
